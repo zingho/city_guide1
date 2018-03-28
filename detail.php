c47ccbeeece50c99d5303e967128c9a5716370d1
zingho <31928598+zingho@users.noreply.github.com>
2018-03-28 16:25:46 +0200

    Zwischenschritt Einzelseiten

---
+++ detail.php
@@ -0,0 +1,74 @@
+﻿<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fussballligen</title>
+    <link href="../style.css" rel="stylesheet">
+  </head>
+  <body>
+  <div id="wrapper">
+      <div id="header"><a href="../index.php"><img src="../logo.png"></a>
+	  
+	<!-- Suche --> 
+	<form action="../suche.php" method="post" id="search">
+		<table align="center" width="200">
+			<tr>
+				<td width="60%"><input name="name" type="text" maxlength="255" size="20" /></td>
+			</tr>
+		</table>
+	</form>
+	
+	<!-- Navigation -->
+	<ul>
+		<li><a href="../index.php">Home</a></li>
+		<li><a href="../privat.php">Ligen bearbeiten</a></li>
+		<li><a href="../registrierung.php">Registrierung</a></li>
+		<li><a href="../login.php">Login</a></li>
+	</ul>		  
+	  
+  </div>
+	 
+	<!-- Textbereich --> 
+	<div id="content" align="center">
+	<?php
+	
+	include('../db_connect.php');
+		
+	$lid = $_GET['lid'];
+
+
+	$sql = "
+	SELECT DISTINCT * 
+	FROM ligen
+	WHERE
+	ID = '$lid'
+	";
+
+	$result = $connect->query($sql);
+	echo '<table border="1">';
+	while($zeile = $result->fetch_assoc()){
+	echo "<tr>";
+	echo '<td>'.'<img src="' . $zeile['Bild'] . '" height="150px"> </td>';
+	echo "<td>". $zeile['Name'] . "</a></td>";
+	echo '<td id="beschreibung"> '. $zeile['Beschreibung'] . "</td>";
+	echo "<td>". $zeile['Gruendungsjahr'] . "</td>";
+	echo "</tr>";
+    }
+	echo "</table>";
+	
+	
+	?>
+	
+	<button onclick="goBack()" class="knopf">Zurück</button>
+
+	<script>
+	function goBack() {
+		window.history.back();
+	}
+	</script>
+	
+	</div>
+	
+  </body>
+</html>
---
+++ seiten.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Creative - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">City Guide</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Über uns</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Städte</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Kontakt</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Bei uns finden Sie!</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+		<?php
+			
+			$stadt = $_GET['stadt'];
+			
+			include('db_connect.php');
+
+			$sql = "
+			SELECT DISTINCT * 
+			FROM staedte
+			WHERE
+			Name = '$stadt'
+			";
+
+			$result = $connect->query($sql);
+			echo '<table border="1">';
+			while($zeile = $result->fetch_assoc()){
+			echo "<tr>";
+			echo "<td>". $zeile['Name'] . "</td>";
+			echo "<td>". $zeile['Beschreibung'] . "</td>";			
+			echo "</tr>";
+			}
+			echo "</table>";
+			
+			?>
+        </div>
+      </div>
+    </section>
+
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  </body>
+
+</html>
